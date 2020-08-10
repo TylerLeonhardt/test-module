b67157296437d7d4701e7dc901a29f0fa365355a
Justin Chen <t-chenju@microsoft.com>
2020-08-10 12:43:03 -0700

    add wait, more prints

--- test/test-module.Tests.ps1
+++ test/test-module.Tests.ps1
@@ -4,6 +4,10 @@ $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         Write-Host $pid
+        while (-not (get-runspace -id 1).debugger.IsActive) {
+            $p = $pid
+            sleep 1
+        };
         Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
         $? | Should Be $true
     }
