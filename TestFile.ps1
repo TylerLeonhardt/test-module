3d13200fc66889223e093d18a9281f1f8d1dfa06
Justin Chen <t-chenju@microsoft.com>
2020-08-10 15:33:18 -0700

    no sleep loop

--- TestFile.ps1
+++ TestFile.ps1
@@ -3,7 +3,3 @@ Write-Host "pid is $id in TestFile"
 
 while (-not (get-runspace -id 1).debugger.IsActive) {sleep 1};
 $a = 0
-for($i = 0; $i -lt 200; $i++){ 
-    $a = $a + $i;
-    Start-Sleep -s 1;
-}
